45c5d549811558e32142e7d4df70072aff41abb0
Gregory Basket <33025713+majerzanka@users.noreply.github.com>
2017-10-23 15:13:13 +0200

    Add files via upload

--- db.php
+++ db.php
@@ -4,14 +4,13 @@ class db
 {
 		private $sql;
 		private $wynik;
-		private  $nazwa_tabeli;
+		private $nazwa_tabeli;
 		private $nazwa_pola;
 		private $wartosc_pola;
 		private $host;
 		private $login;
 		private $password;
 		private $database;
-		private $error;
 		private $tablica;
 		private $link;
 
@@ -34,17 +33,30 @@ class db
 			
 			
 			
-			$this->link = mysqli_connect($host , $login , $password , $database);
+			$this->link = mysqli_connect($this->host , $this->login , $this->password , $this->database);
 			
 			if(!$this->link)
 			{
 				printf("blad polaczenia z baza danych");
-				return $this->error = mysqli_connect_error($this->link);
+				$error =  mysqli_connect_error($this->link);
+				return $error;
 				
 			}
 			
 			return $this->link;
 		}
+		
+		/*function debug($link)
+		{
+			$this->sql = $link;
+			$this->wynik = mysqli_query($this->sql , "SELECT * FROM produkty");
+			$this->tablica = mysqli_fetch_array($this->wynik);
+			
+			print_r($this->tablica[0]);
+			
+		}
+		*/
+		
 		
 		public function insert($link)
 		{
@@ -75,31 +87,21 @@ class db
 			
 		}
 		
-		public function fetch($link)
+		public function fetchAll($link)
 		{
 			$this->sql = $link;
-			$this->wynik = mysqli_query($this->sql , "SELECT * FROM produkty");
+			$this->wynik = mysqli_query($this->sql , "select * from produkty");
+			
 			
 			if(!$this->wynik)
 				return false;
 			
-			return $this->tablica = mysqli_fetch_array($this->wynik);
+			$cal = array();
 			
-			
-			
-			
-		}
+			while($this->tablica = mysqli_fetch_array($this->wynik))
+				$cal[] = $this->tablica;
 		
-		public function fetchAll($link , $id)
-		{
-			$this->sql = $link;
-			$this->wynik = mysqli_query($this->sql , "SELECT * FROM produkty where" . $id = "0");
-	
-	
-			if(!$this->wynik)
-				return false;
-			
-			return $this->tablica = mysqli_fetch_array($this->wynik);
+			return $cal;
 			
 			
 			
@@ -110,39 +112,66 @@ class db
 		{
 			$this->sql = $link;
 			$this->wynik = mysqli_query($this->sql , "SELECT * FROM produkty");
+	
+	
+			if(!$this->wynik)
+				return false;
+			
+			$this->tablica = mysqli_fetch_array($this->wynik);
+			
+			
+			if(!$this->tablica)
+				return false;
+			
+			
+			return $this->tablica[0];
+			
+			
+			
+			
+		}
+		
+		public function fetch($link)
+		{
+			$this->sql = $link;
+			$this->wynik = mysqli_query($this->sql , "SELECT * FROM produkty");
 			
 			$this->tablica = mysqli_fetch_array($this->wynik);
 			
 			if(!$this->wynik)
 				return false;
 			
-			return $this->tablica[0];
+			if(!$this->tablica)
+				return false;
+			
+			return $this->tablica;
 			
 			
 		}
 		
 		
-		public function fetchPrint($link , $id)
+		public function fetchPrint($link)
 		{
 			$this->sql = $link;
-			$this->wynik = mysqli_query($this->sql , "SELECT * FROM produkty where"
-			. $id = "0");
+			$this->wynik = mysqli_query($this->sql , "SELECT * FROM produkty");
 			
 			if(!$this->wynik)
 				return false;
 			
-			printf("<table>");
-			echo "<tr>";
+			$this->tablica = mysqli_fetch_array($this->wynik);
 			
-			foreach($this->wynik as $val)
-			{
-				echo "<td>";
-					echo $val;
-				echo "</td>";
-			}
+			if(!$this->tablica)
+				return false;
 			
-			echo "<tr>";
-			printf("</table>");
+			echo "<table border = 1>";
+			foreach($this->tablica as $key => $val)
+				if(is_numeric($key))
+				{
+					echo "<tr>";
+						echo "<td>";
+							echo $val;
+						echo "</td>";
+				}
 			
 		}
 		
@@ -153,19 +182,21 @@ class db
 			
 			if(!$this->wynik)
 				return false;
+			$this->tablica = mysqli_fetch_array($this->wynik);
 			
-			printf("<table>");
-			echo "<tr>";
+			if(!$this->tablica)
+				return false;
 			
-			foreach($this->wynik[0] as $val)
-			{
-				echo "<td>";
-					echo $val;
-				echo "</td>";
-			}
+			echo "<table border = 1>";
+			foreach($this->tablica as $key => $val)
+				if(is_numeric($key) && $key == 0)
+				{
+					echo "<tr>";
+						echo "<td>";
+							echo $val  . "<br>";
+						echo "</td>";
+				}
 			
-			echo "<tr>";
-			printf("</table>");
 			
 			
 		}
@@ -178,19 +209,43 @@ class db
 			if(!$this->wynik)
 				return false;
 			
-			printf("<table>");
-			echo "<tr>";
 			
-			foreach($this->wynik as $val)
+			echo "<table border = 1>";
+			while($this->tablica = mysqli_fetch_array($this->wynik))
 			{
-				echo "<td>";
-					echo $val;
-				echo "</td>";
+				echo "<tr>";
+					echo "<td>";
+						echo $this->tablica[0];
+					echo "</td>";
+					
+					echo "<td>";
+						echo $this->tablica[1];
+					echo "</td>";
+					echo "<td>";
+						echo $this->tablica[2];
+					echo "</td>";
+					echo "<td>";
+						echo $this->tablica[3];
+					echo "</td>";
+					echo "<td>";
+						echo $this->tablica[4];
+					echo "</td>";
+					echo "<td>";
+						echo $this->tablica[5];
+					echo "</td>";
+					echo "<td>";
+						echo $this->tablica[6];
+					echo "</td>";
+					echo "<td>";
+						echo $this->tablica[7];
+					echo "</td>";
+				echo "</tr>";
+			
+			
+				
 			}
-			
-			echo "<tr>";
-			printf("</table>");
-			
+			echo "</table>";
+				
 		}
 		
 		public function close($link)
@@ -201,13 +256,25 @@ class db
 				
 		public function __Destruct()
 		{
-			$this->sql = $link;
-			mysqli_close($this->sql);
+			
 		}
+		
 		
 
 
 }
+
+$db = new db;
+
+$con = $db->open("localhost" , "root" , "" , "3i_sklep");
+
+$wyn = $db->fetchPrintOne($con);
+
+print_r($wyn)
+
+
+
+
 
 
 
